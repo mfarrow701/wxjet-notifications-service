b796bf0b56ae1dadeb011f6d33d681e12862b766
Matthew Farrow <matthew.farrow@metoffice.gov.uk>
2018-05-14 15:04:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+<p align="center">
+  <a href="https://github.com/mfarrow701/wxjet-notifications-service" target="_blank">
+    <img alt="WxJet Notifications Service" title="WxJet Notifications Service" src="https://github.com/mfarrow701/wxjet/blob/master/logo.png" width="180">
+  </a>
+</p>
+
+<h1 align="center"> WxJet Notifications Service</h1> 
+
+<div align="center">
+  <!-- Cats -->
+ <a href="">
+    <img src="https://forthebadge.com/images/badges/contains-cat-gifs.svg"
+      alt="Contains cats" title="Contains cats" />
+  </a>
+  
+  <!-- JavaScript -->
+ <a href="">
+    <img src="https://forthebadge.com/images/badges/made-with-javascript.svg"
+      alt="Made with JavaScript" title="Made with JavaScript" />
+  </a>
+  
+  <!-- With love -->
+   <a href="">
+      <img src="https://forthebadge.com/images/badges/built-with-love.svg"
+        alt="Made with love" title="Made with love" />
+    </a>
+  
+</div>
+
+<p align="center">
+   <strong> A simple node JS notifications service for WxJet, demonstrating server-sent events &amp; websockets</strong>
+</p>
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+To clone and run this application, you'll need Git, Node.js and npm install. You can also use an alternative package manager, such as Yarn if you prefer!
+
+### Installing
+
+From your favourite command line tool, run the following:
+
+```
+# Clone the repo
+git clone ssh://git@github.com:mfarrow701/wxjet-notifications-service.git
+
+# Install dependencies
+yarn install
+
+# Run the application
+yarn start
+```
+
+Once you have ensured that the service is up and running (by navigating to http://localhost:3001/) spin up a copy of WxJet to see the magic happen!
+
+## Authors
+
+* **Matthew Farrow** - *Initial work* 
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ app.js
@@ -0,0 +1,84 @@
+const app = require('express')(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server),
+    moment = require('moment'),
+    uuid4 = require('uuid/v4');
+
+io.set('origins', '*:*');
+server.listen(3001);
+
+const warnings = ['Cloud', 'Wind', 'Visibility'], conditions = ['dry', 'damp', 'wet', 'flooded'],
+    warningStates = ['#9932CC', '#FF0000', '#FF8C00', '#008000'], conditionStates = ['#ff1493', '#40e0d0', '#ff9800', '#607d8b'];
+
+app.get('/', (req, res) => {
+    res.send('<h1>WxJet Notifications Service</h1>')
+});
+
+app.get('/sse-notifications', (req, res) => {
+    console.log('An application has subscribed to the SSE notifications service');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Expose-Headers', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    });
+    setInterval(() => {
+        generateSEENotification(req, res);
+    }, 15000);
+    generateSEENotification(req, res);
+});
+
+io.on('connection', function (socket) {
+    console.log('Connection from ' + socket.handshake.headers.host + ' received at: ' + socket.handshake.time);
+
+    socket.on('subscribeToNotifications', () => {
+        console.log('An application has subscribed to the socket notifications service');
+        setInterval(() => {
+            socket.emit('notification', generateSocketNotification());
+        }, 12000);
+        socket.emit('notification', generateSocketNotification());
+    });
+
+    socket.on('disconnect', () => {
+        console.log('An application has un-subscribed to the notifications service');
+    })
+});
+
+const generateSEENotification = (req, res) => {
+    const warningId = uuid4(),
+        data = {
+            message: 'Runway condition is ' + conditions[Math.floor(Math.random() * conditions.length)],
+            colourState: conditionStates[Math.floor(Math.random() * conditionStates.length)]
+        };
+    res.write('id: ' + warningId + '\n');
+    res.write('data: ' + JSON.stringify(data) + '\n\n');
+};
+
+const generateSocketNotification = () => {
+    const warningId = uuid4(),
+        dateTimeNow = moment().format('MMMM Do YYYY, h:mm:ss a'),
+        notifications = [{
+            warningId: warningId,
+            date: dateTimeNow,
+            colourState: warningStates[0],
+            message: warnings[Math.floor(Math.random() * warnings.length)] + ' threshold limits exceeded'
+        }, {
+            warningId: warningId,
+            date: dateTimeNow,
+            colourState: warningStates[1],
+            message: warnings[Math.floor(Math.random() * warnings.length)] + ' threshold limits reached'
+        }, {
+            warningId: warningId,
+            date: dateTimeNow,
+            colourState: warningStates[2],
+            message: warnings[Math.floor(Math.random() * warnings.length)] + ' threshold limits marginal'
+        }, {
+            warningId: warningId,
+            date: dateTimeNow,
+            colourState: warningStates[3],
+            message: warnings[Math.floor(Math.random() * warnings.length)] + ' threshold limits OK'
+        }];
+    return notifications[Math.floor(Math.random() * notifications.length)];
+};
